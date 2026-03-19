4da458fc4ccd327b9dc0c2b069c7ce16060f1fc3
zsd <pavlovich5703@gmail.com>
2026-03-19 10:56:47 +0300

    fix bi-directional UserRating<->Movie relation changed to single-directional UserRating->Movie relation

--- backend/src/users/infrastructure/db/orm.py
+++ backend/src/users/infrastructure/db/orm.py
@@ -104,7 +104,7 @@ class ListMovie(Base):
         nullable=False,
         primary_key=True,
     )
-    movie: Mapped[Movie] = relationship("Movie", backref="list_movies", lazy="joined")
+    movie: Mapped[Movie] = relationship("Movie", lazy="joined")
 
     created_at: Mapped[datetime] = mapped_column(
         server_default=text("now()"), nullable=False
@@ -176,7 +176,7 @@ class UserRating(Base):
         nullable=False,
         primary_key=True,
     )
-    movie: Mapped[Movie] = relationship("Movie", backref="user_ratings", lazy="joined")
+    movie: Mapped[Movie] = relationship("Movie", lazy="joined")
 
     rating: Mapped[int] = mapped_column(index=True)
 
